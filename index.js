f0112813b40ebbe0ef3e00676e84be47c2d0248f
JhonniPinto <jhonnipinto@gmail.com>
2019-06-03 15:54:43 -0300

    Removi comentário teste e adicionei a porta como variável 'env'

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser')
 const sqlite = require('sqlite')
 const dbConnection = sqlite.open('banco.sqlite', { Promise })
 
+const port = process.env.PORT || 3000
+
 app.set('view engine', 'ejs')     // configuração para que possa ser usado o ejs com o express
 app.use(express.static('public')) // configuração que permite você acessar, através de servidor, arquivos dentro da pasta public, como com o caminho '/img/logo.png'
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -124,10 +126,9 @@ const init = async() => {
     // await db.run(`insert into vagas(categoria, titulo, descricao) values(3, '${vaga}', '${descricao}')`)
 }
 init()
-// comentário
 // app.listen(3000) é um serviço do express() chamado de ouvinte, que abre uma porta no nosso computador para a comunicação via servidor, onde vamos ouvir o resquest e retornar a response tais informações
 // portas até 1000, são portas privilegiadas, que são usadas para a publicação do projeto. Porta 80 para http e 443 para https, normalmente. Portas como 3000 ou 8080 são portas usadas para desenvolvimento, pois são menos monitoradas por firewalls, por exemplo, que poderiam complicar.
-app.listen(8000, (err) => {
+app.listen(port, (err) => {
     if (err) {
         console.log('Não foi possível iniciar o servidor do JobiFy. Erro:', err)
     } else {
